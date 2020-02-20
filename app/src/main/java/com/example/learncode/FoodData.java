925efe03f34a1c9091bf3daf37140346ffdfed72
Keyr Mistry <keyurmistry96@gmail.com>
2020-02-20 11:09:40 -0500

    Home Fragment

---
+++ app/src/main/java/com/example/learncode/FoodData.java
@@ -0,0 +1,42 @@
+package com.example.learncode;
+
+public class FoodData {
+
+    private String name;
+    private String description;
+    private String price;
+    private int image;
+
+    public FoodData(String name, String description, String price, int image) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public String getPrice() {
+        return price;
+    }
+
+
+    public int getImage() {
+        return image;
+    }
+
+
+
+
+
+
+
+}
