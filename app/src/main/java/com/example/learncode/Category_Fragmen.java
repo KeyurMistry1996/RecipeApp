3fe87c14eef62a33ed1243e7e2f4ee21ea603584
Keyr Mistry <keyurmistry96@gmail.com>
2020-02-21 10:41:56 -0500

    Grid

--- app/src/main/java/com/example/learncode/Category_Fragmen.java
+++ app/src/main/java/com/example/learncode/Category_Fragmen.java
@@ -1,6 +1,7 @@
 package com.example.learncode;
 
 
+import android.R.layout;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Category_Fragmen extends Fragment {
-
+    GridView gridView;
+    GridAdapte gridAdapter;
 
     public Category_Fragmen() {
         // Required empty public constructor
@@ -25,7 +33,19 @@ public class Category_Fragmen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category_, container, false);
+        View view =  inflater.inflate(R.layout.fragment_category_, container, false);
+        int[] arrayListImage = {R.drawable.background,R.drawable.background,R.drawable.background,R.drawable.background,R.drawable.background,R.drawable.background};
+        String[] arrayListITitle = {"Title","Title","Title","Title","Title","Title"};
+
+
+         gridView = view.findViewById(R.id.gridview);
+
+        gridAdapter = new GridAdapte(this,arrayListITitle,arrayListImage);
+        gridView.setAdapter(gridAdapter);
+        return view;
     }
+
+
+
 
 }
---
+++ app/src/main/java/com/example/learncode/GridAdapte.java
@@ -0,0 +1,55 @@
+package com.example.learncode;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+
+class GridAdapte extends BaseAdapter {
+    Category_Fragmen context;
+    String[] arrayListTitle;
+    int[] arrayListImages;
+
+
+    public GridAdapte(Category_Fragmen category_fragmen, String[] arrayListITitle, int[] arrayListImage) {
+
+       this.context = category_fragmen;
+       this.arrayListTitle = arrayListITitle;
+       this.arrayListImages = arrayListImage;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayListImages.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return arrayListTitle.length;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_model,parent,false);
+        ImageView imageView = convertView.findViewById(R.id.gridImage);
+        TextView textView = convertView.findViewById(R.id.gridTitle);
+        imageView.setImageResource(arrayListImages[position]);
+        textView.setText(arrayListTitle[position]);
+        return convertView;
+    }
+
+
+}
